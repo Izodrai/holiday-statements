1ec7113a646fb0bb0b5560d2b85d58f15c995b8f
Valentin Perraudin <vap@capitaldata.fr>
2016-08-19 19:40:17 +0200

    clear files and add first steps for update an user

--- lib/routes/users/handler.go
+++ lib/routes/users/handler.go
@@ -1,17 +1,10 @@
 package users
 
 import (
-// 	tmpl "../../templates"
-// 	"../../tools"
 	"github.com/abbot/go-http-auth"
 	"net/http"
 	
 	wu "../../workers/users"
-	
-// 	"fmt"
-// 	"github.com/abbot/go-http-auth"
-// 	"golang.org/x/crypto/bcrypt"
-// 	"net/http"
 )
 
 func HandleUsers(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
@@ -23,10 +16,10 @@ func HandleUsers(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
 		return 
 	}
 	
+// 	if _, ok := params["update"]; ok {
+// 		wu.Update(w,r)
+// 		return 
+// 	}
+	
 	wu.List(w,r)
-// 	tools.Info("users")
-// 	b, _ := bcrypt.GenerateFromPassword([]byte("vp"), 10)
-// 	tools.Info(string(b))
-// 
-// 	fmt.Fprint(w, `<html>`+/*tools.Menu+*/`<p>users</p>`, r.Username)
 }
