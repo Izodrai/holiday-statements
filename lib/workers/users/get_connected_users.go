469c06fad13d687822b260ec8eee581b74f3ea4a
Izodrai <vap@capitaldata.fr>
2016-09-29 19:41:09 +0200

    add Get_connected_users func and routes

---
+++ lib/workers/users/get_connected_users.go
@@ -0,0 +1,39 @@
+package users
+
+import (
+// 	"fmt"
+// 	"strconv"
+	"net/http"
+	"../../authentification"
+	"../../tools"
+	"github.com/gin-gonic/gin"
+)
+
+/****
+* http://localhost:8080/users/get_connected
+* curl -i -X POST -d '{"user_id":<1>, "token":"<token>"}' http://localhost:8080/users/get_connected
+****/
+
+func Get_connected_users(c *gin.Context) {
+	
+	var json tools.Request
+	
+	if !authentification.Check_token(c, &json) {
+		return
+	}
+	
+	var cus []tools.User
+	
+	for _, cu := range tools.Connected_users {
+		cu.Email = ""
+		cu.Password = ""
+		cu.Token = ""
+		cus = append(cus,cu)
+	}
+	
+	c.JSON(http.StatusOK, gin.H{
+		"users": cus,
+		"code": http.StatusOK,
+		"msg": "users found",
+	})
+}
